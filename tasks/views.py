084913a7660a3a3fd58eb100d2cc5a71075aa48a
Grzegorz Latuszek <latuszek.grzegorz@gmail.com>
2022-01-15 14:06:20 +0100

    index view using Task model, tasks loop inside list template
    To see change you need manually enter some tasks in DB
    since we have no other way to create task yet.

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
+
+from .models import Task
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'tasks/list.html')
+    tasks = Task.objects.all()
+
+    context = {'tasks': tasks}
+    return render(request, 'tasks/list.html', context)
