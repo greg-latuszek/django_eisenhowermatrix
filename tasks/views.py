e4700de728af5fa8ce6ee2da811460e9d189ebe7
Grzegorz Latuszek <latuszek.grzegorz@gmail.com>
2022-01-15 14:50:20 +0100

    handle posting form inside same view "index"
    need declaring form method&url in template
    need conditional handling inside index - for after-POST action

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.shortcuts import render, redirect
+# from django.views.generic import ListView, DetailView
 
 from .models import Task
 from .forms import TaskForm
@@ -11,6 +11,12 @@ def index(request):
     tasks = Task.objects.all()
 
     form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()  # will write to DB
+        return redirect('/')
 
     context = {'tasks': tasks, 'form': form}
     return render(request, 'tasks/list.html', context)
