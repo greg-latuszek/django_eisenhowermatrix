e617b8269f2813ec45271dae2234d9eb818cc3da
Grzegorz Latuszek <latuszek.grzegorz@gmail.com>
2022-01-15 14:34:20 +0100

    create form related to model, pass that form from view, use it inside template

--- tasks/views.py
+++ tasks/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 
 from .models import Task
+from .forms import TaskForm
 
 # Create your views here.
 
@@ -9,5 +10,7 @@ from .models import Task
 def index(request):
     tasks = Task.objects.all()
 
-    context = {'tasks': tasks}
+    form = TaskForm()
+
+    context = {'tasks': tasks, 'form': form}
     return render(request, 'tasks/list.html', context)
