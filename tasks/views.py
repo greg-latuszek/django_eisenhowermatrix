2caebce588a7e0778c819c5cbc6ba45fe0afc66b
Grzegorz Latuszek <latuszek.grzegorz@gmail.com>
2022-01-16 10:16:20 +0100

    handling POST in delete_task
    need csrf and submit-input in template (but not form)
    and removal from DB in delete_task view

--- tasks/views.py
+++ tasks/views.py
@@ -40,5 +40,9 @@ def update_task(request, pk):
 def delete_task(request, pk):
     item = Task.objects.get(id=pk)
 
+    if request.method == "POST":
+        item.delete()
+        return redirect('/')
+
     context = {'item': item}
     return render(request, 'tasks/delete.html', context)
