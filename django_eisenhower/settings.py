803c75f3ef167e901740baaf20f9bde613eec6f2
Grzegorz Latuszek <latuszek.grzegorz@gmail.com>
2022-01-28 02:47:06 +0100

    activate login: /accounts/login

--- django_eisenhower/settings.py
+++ django_eisenhower/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^ajn!iu3(+z5#s(!$vl1r!d5d=w6dsqw@20pr%#*0k^l8vfcum
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.1.231']
 
 
 # Application definition
@@ -123,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
