59cec9080a8c588672105703e3e072ccc8089ab3
Grzegorz Latuszek <latuszek.grzegorz@gmail.com>
2022-03-08 19:21:18 +0100

    add handling of favicon.ico
    https://simpleit.rocks/python/django/django-favicon-adding/

--- django_eisenhower/urls.py
+++ django_eisenhower/urls.py
@@ -15,10 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('tasks.urls')),
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url=staticfiles_storage.url("img/favicon.ico")),
+    ),
 ]
