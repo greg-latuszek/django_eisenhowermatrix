ebabc5dd1379a40dbb41b1d47eed592705b06259
Grzegorz Latuszek <latuszek.grzegorz@gmail.com>
2022-03-16 19:57:06 +0100

    DRF using viewsets
    check:
    curl -s http://127.0.0.1:8007/api/ | python3 -m json.tool
    curl -s http://127.0.0.1:8007/api/tasks/ | python3 -m json.tool

--- django/tasks/urls.py
+++ django/tasks/urls.py
@@ -1,10 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
+
+router = DefaultRouter()
+router.register(r"tasks", views.TasksViewSet, "tasks")
 
 urlpatterns = [
     path("", views.index, name="tasks_list"),
     path("update_task/<str:pk>/", views.update_task, name="update_task"),
     path("delete/<str:pk>/", views.delete_task, name="delete_task"),
     path("prime/<int:x>/", views.show_primes, name="show_primes"),
-    path("api/list_tasks/", views.list_tasks, name="api_list_tasks"),
+    path("api/", include(router.urls))
 ]
--- django/tasks/views.py
+++ django/tasks/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render, redirect
-# from django.views.generic import ListView, DetailView
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from rest_framework import viewsets
 
 from .models import Task
 from .forms import TaskForm
@@ -65,11 +63,8 @@ def show_primes(request, x):
 
 
 # DRF
-@api_view(["GET"])
-def list_tasks(request):
-    tasks = Task.objects.all()
-    serializer = TaskSerializer(tasks, many=True)
-    content = {
-        "tasks": serializer.data,
-    }
-    return Response(content)
+class TasksViewSet(viewsets.ModelViewSet):
+    serializer_class = TaskSerializer
+
+    def get_queryset(self):
+        return Task.objects.all()
