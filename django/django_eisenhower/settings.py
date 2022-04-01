7d5598a059e653284e385c2994248f9fe657ae72
Grzegorz Latuszek <latuszek.grzegorz@gmail.com>
2022-04-01 13:50:46 +0200

    install bandit as pre-commit hook
    Move SECRET_KEY out of settings.py (into .env), see:
    https://docs.djangoproject.com/en/4.0/topics/signing/

--- django/django_eisenhower/settings.py
+++ django/django_eisenhower/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-^ajn!iu3(+z5#s(!$vl1r!d5d=w6dsqw@20pr%#*0k^l8vfcum"
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
